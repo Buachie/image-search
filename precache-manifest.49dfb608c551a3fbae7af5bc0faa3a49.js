21ce4fc3ad1416c3e279a45db99a24ce99e048d5
Aaron Buachie <aaron.buachiea@gmail.com>
2020-07-25 14:53:06 -0400

    Updates

---
+++ precache-manifest.49dfb608c551a3fbae7af5bc0faa3a49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84daaaefa8b6e44b537e9088ebb5a80f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6c6f3ba05b237eda841",
+    "url": "/static/css/main.341b0230.chunk.css"
+  },
+  {
+    "revision": "7fd15d9547cd32944426",
+    "url": "/static/js/2.0b8d8d44.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/static/js/2.0b8d8d44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6c6f3ba05b237eda841",
+    "url": "/static/js/main.a66e513f.chunk.js"
+  },
+  {
+    "revision": "bc2144d91206f9fab785",
+    "url": "/static/js/runtime-main.e4912f7f.js"
+  }
+]);
