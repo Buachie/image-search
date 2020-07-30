07db0d2da99045d9490bcef5e9bc71cf9ec25df6
Aaron Buachie <aaron.buachiea@gmail.com>
2020-07-30 08:23:01 -0400

    Add build folder

---
+++ build/precache-manifest.4ac4acb479cc5c2f0451fd6d3ea44dd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef9f95d8f9b09d6e5039161d895ab0f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "07c28ad40ec4f5818901",
+    "url": "/static/css/main.050077c8.chunk.css"
+  },
+  {
+    "revision": "bae8e5c3c7f7939f279c",
+    "url": "/static/js/2.45ffd1fa.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/static/js/2.45ffd1fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "07c28ad40ec4f5818901",
+    "url": "/static/js/main.35a894c3.chunk.js"
+  },
+  {
+    "revision": "bc2144d91206f9fab785",
+    "url": "/static/js/runtime-main.e4912f7f.js"
+  }
+]);
